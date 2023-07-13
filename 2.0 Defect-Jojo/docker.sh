c20f921fb6caf6fd55475d86cf61b7403661d078
adesinna <aladesaea@yahoo.com>
2023-07-13 14:15:31 +0100

    defect jojo

---
+++ 2.0 Defect-Jojo/docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Install Docker
+# Update the package index
+sudo apt update
+
+# Install Docker's dependencies
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+# Add the Docker GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Add the Docker repository
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+
+# Install Docker Compose version 1.28.0
+# Download the Docker Compose binary
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Apply executable permissions to the binary
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Verify the installation
+docker-compose --version
