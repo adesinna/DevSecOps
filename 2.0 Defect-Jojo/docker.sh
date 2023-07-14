27e4a662fac8913e89e82745f87eb65029373865
adesinna <aladesaea@yahoo.com>
2023-07-14 13:11:33 +0100

    worked on https of defectjojo

--- 2.0 Defect-Jojo/docker.sh
+++ 2.0 Defect-Jojo/docker.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
-# Install Docker
-# Update the package index
-sudo apt update
+# Update the system
+sudo apt update -y
 
-# Install Docker's dependencies
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+# Install Docker dependencies
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-# Add the Docker GPG key
+# Add Docker GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-# Add the Docker repository
+# Add Docker repository
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# Update the package lists
+sudo apt update -y
+
 # Install Docker
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Add the current user to the "docker" group
+sudo usermod -aG docker $USER
 
 # Install Docker Compose version 1.28.0
-# Download the Docker Compose binary
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-# Apply executable permissions to the binary
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Verify the installation
+# Print Docker and Docker Compose versions
+docker --version
 docker-compose --version
+
+# Allow the current user to execute Docker commands without sudo
+echo "Please log out and log back in to apply the user group changes."
